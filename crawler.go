b8b0255897464e8b1593bc2b6b82f7b54731e9df
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2016-10-09 10:03:22 +0100

    Cleanup

--- crawler.go
+++ crawler.go
@@ -15,102 +15,93 @@ var builder bytes.Buffer
 var visited = make(map[string]bool)
 
 type Crawler struct {
-
-    startUrl string
-
+    startUrl *url.URL
     host string
-
-    regex *regexp.Regexp
-
+    urlRegex *regexp.Regexp
     assetRegex *regexp.Regexp
 }
 
 type Page struct {
-    location string
-
-    links []string
-
+    location *url.URL
+    links []*url.URL
     assets []*url.URL
 }
 
-func (crawler *Crawler) run() string {
-    buildStart()
-    crawler.crawl(crawler.startUrl)
-    buildEnd()
-
-    return builder.String()
-}
-
-func (crawler *Crawler) crawl(currentUrl string) {
-    response, error := http.Get(currentUrl)
+func (crawler *Crawler) crawl(url *url.URL) {
+    response, error := http.Get(url.String())
     
     if error != nil {
-        fmt.Println("Failed GET to: ", currentUrl, " error: ", error)
-    } else {
-        fmt.Println("Fetching: ", currentUrl)
-        body, error := ioutil.ReadAll(response.Body)
-        if error != nil {
-            fmt.Println("Failed to read response body, error: ", error)
-        } else {
-            urls := crawler.extractUrls(currentUrl, string(body))
-            for _, url := range urls {
-                crawler.crawl(url)
-            }
-        }
-
-        response.Body.Close()
+        fmt.Println("Failed GET to:", url, "exiting. Error: was", error)
+        return
     }
+
+    fmt.Println("Fetching:", url)
+
+    body, error := ioutil.ReadAll(response.Body)
+    if error != nil {
+        fmt.Println("Failed to read response body, exiting. Error was: ", error)
+        return
+    }
+
+    subUrls := crawler.getUrlsFromBody(url, string(body))
+    for _, subUrl := range subUrls {
+        crawler.crawl(subUrl)
+    }
+
+    response.Body.Close()
 }
 
-func (crawler *Crawler) extractUrls(stringUrl, body string) []string {
-    urls := make([]string, 0)
+func (crawler *Crawler) getUrlsFromBody(parentUrl *url.URL, body string) []*url.URL {
+    subUrls := make([]*url.URL, 0)
+    subUrlMatches := getUnpackedMatches(crawler.urlRegex, body)
+    assetMatches := getUnpackedMatches(crawler.assetRegex, body)
 
-    newUrls := crawler.regex.FindAllStringSubmatch(body, -1)
-    pageAssets := crawler.assetRegex.FindAllStringSubmatch(body, -1)
+    if subUrlMatches != nil && assetMatches != nil {
+        parsedAssets := parseUrls(assetMatches, parentUrl)
+        parsedUrls := parseUrls(subUrlMatches, parentUrl)
 
-    currentUrl, _ := url.Parse(stringUrl)
-
-    if newUrls != nil {
-        parsedUrls := parseUrls(newUrls, currentUrl)
-
-        for _, newUrl := range parsedUrls { 
-            if shouldVisit(newUrl, crawler.host) {
-                urls = append(urls, newUrl.String())
+        for _, subUrl := range parsedUrls { 
+            if shouldVisit(subUrl, crawler.host) {
+                visited[subUrl.String()] = true
+                subUrls = append(subUrls, subUrl)
             }
         }
 
-        parsedAssets := parseUrls(pageAssets, currentUrl)
-
         page := Page {
-            stringUrl,
-            urls,
+            parentUrl,
+            subUrls,
             parsedAssets,
         };
 
         addPageToSiteMap(page)
     }
 
-    return urls
+    return subUrls
+}
+
+func getUnpackedMatches(regex *regexp.Regexp, content string) []string {
+    result := regex.FindAllStringSubmatch(content, -1)
+
+    unpacked := make([]string, 0)
+    for _, match := range result {
+        unpacked = append(unpacked, match[1])
+    }
+
+    return unpacked
 }
 
 func shouldVisit(url *url.URL, startHost string) bool {
     newUrlHost := getNormalizedHost(url.Host)
-    shouldVisit := newUrlHost == startHost && !visited[url.String()]
-    visited[url.String()] = true
-    return shouldVisit
+    return newUrlHost == startHost && !visited[url.String()]
 }
 
-func parseUrls(rawUrls [][]string, currentUrl *url.URL) []*url.URL {
+func parseUrls(rawUrls []string, currentUrl *url.URL) []*url.URL {
     urls := make([]*url.URL, 0)
 
     for _, raw := range rawUrls {
-        stringUrl := raw[1];
-        newUrl, error := url.Parse(stringUrl)
-        if error != nil {
-            continue
-        }
-
+        newUrl, _ := url.Parse(raw)
         newUrl = makeAbsolute(currentUrl, newUrl)
+        
         if (isValidUrl(newUrl)) {
             urls = append(urls, newUrl)
         }
@@ -151,44 +142,54 @@ func addPageToSiteMap(page Page) {
 
     // add location
     builder.WriteString("    <loc>")
-    builder.WriteString(page.location)
+    builder.WriteString(page.location.String())
     builder.WriteString("</loc>\n")
 
     // add links
     builder.WriteString("    <links:links>\n")
-    for _, link := range page.links {
-        builder.WriteString("      <link>")
-        builder.WriteString(link)
-        builder.WriteString("</link>\n")
-    }
+    buildTagSection("link", page.links)
     builder.WriteString("    </links:link>\n")
 
     //add assets
     builder.WriteString("    <assets>\n")
-    for _, asset := range page.assets {
-        builder.WriteString("      <asset>")
-        builder.WriteString(asset.String())
-        builder.WriteString("</asset>\n")
-    }
+    buildTagSection("asset", page.assets)
     builder.WriteString("    </assets>\n")
 
     builder.WriteString("  </url>\n")
 }
 
+func buildTagSection(tag string, content []*url.URL) {
+    builder.WriteString("    <" + tag + ">\n")
+    for _, info := range content {
+        builder.WriteString("      <" + tag + ">")
+        builder.WriteString(info.String())
+        builder.WriteString("</" + tag + ">\n")
+    }   
+}
+
+func (crawler *Crawler) run() string {
+    buildStart()
+    crawler.crawl(crawler.startUrl)
+    buildEnd()
+
+    return builder.String()
+}
+
 func main() {
     startUrl := os.Args[1]
-    regex := regexp.MustCompile("<a.*?href=\"([^\"]*)\".*?>")
+    urlRegex := regexp.MustCompile("<a.*?href=\"([^\"]*)\".*?>")
     assetRegex := regexp.MustCompile("<link.*?href=\"([^\"]*)\".*?>")
 
-    u, _ := url.Parse(startUrl)
-    host := getNormalizedHost(u.Host)
+    url, _ := url.Parse(startUrl)
+    host := getNormalizedHost(url.Host)
 
     crawler := Crawler {
-        startUrl,
+        url,
         host,
-        regex,
+        urlRegex,
         assetRegex,
     };
 
-    fmt.Printf(crawler.run())
+    siteMap := crawler.run()
+    fmt.Printf("\n" + siteMap)
 }
